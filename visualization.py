6a149ac0571bdd7f5136f78c814e337cdea2b136
Marcio Lima <marcio.lima.inacio@gmail.com>
2021-11-03 10:25:00 -0300

    3d plot of runtime. Save figures. Update results.

--- visualization.py
+++ visualization.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -39,18 +40,39 @@ timeout_df = pd.concat([timeout_counts, non_timeout_counts], axis=1)
 timeout_df.index = ['Code 1', 'Code 2']
 timeout_df.plot.bar()
 plt.tight_layout()
+plt.savefig('img/timeout_hist.pdf', format='pdf')
 
 # Average runtime by probability
 
 runtime_data = df.groupby(by=['algorithm',
+                              'Number of exams',
                               'Overlap probability'])['runtime'].mean()
 code1_runtime = runtime_data['code1']
 code1_runtime.name = 'Code 1'
 code2_runtime = runtime_data['code2']
 code2_runtime.name = 'Code 2'
 
-runtime_df = pd.concat([code1_runtime, code2_runtime], axis=1)
-runtime_df.plot.line(subplots=True)
+fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={'projection': '3d'})
+plt.tight_layout(pad=7)
 
-plt.tight_layout()
-plt.show()
+num_exams = df['Number of exams'].unique()
+for exams in num_exams:
+    code1_data = code1_runtime.xs(exams, level='Number of exams')
+    code2_data = code2_runtime.xs(exams, level='Number of exams')
+
+    ax1.scatter(code1_data.index,  # Probability
+                np.repeat(exams, len(code1_data)),  # Number of exams
+                code1_data)  # Runtime
+    ax2.scatter(code2_data.index,
+                np.repeat(exams, len(code2_data)),
+                code2_data)
+
+ax1.set_xlabel('Overlap probability')
+ax1.set_ylabel('Number os exams')
+ax1.set_zlabel('Runtime (s)')
+
+ax2.set_xlabel('Overlap probability')
+ax2.set_ylabel('Number os exams')
+ax2.set_zlabel('Runtime (s)')
+
+plt.savefig('img/runtime_3d.pdf', format='pdf')
