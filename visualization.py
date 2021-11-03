2695ca9b28ae7a78837ce5942aa730eea50ceb28
Marcio Lima <marcio.lima.inacio@gmail.com>
2021-11-03 18:52:36 -0300

    Clean script and add bar annotation

--- visualization.py
+++ visualization.py
@@ -38,41 +38,10 @@ non_timeout_counts.name = 'Non-timeout'
 
 timeout_df = pd.concat([timeout_counts, non_timeout_counts], axis=1)
 timeout_df.index = ['Code 1', 'Code 2']
-timeout_df.plot.bar()
+ax = timeout_df.plot.bar()
+for p in ax.patches:
+    ax.annotate(str(p.get_height()),
+                (p.get_x() + (p.get_width() * 0.5), p.get_height() * 1.005),
+                ha='center')
 plt.tight_layout()
 plt.savefig('img/timeout_hist.pdf', format='pdf')
-
-# Average runtime by probability
-
-runtime_data = df.groupby(by=['algorithm',
-                              'Number of exams',
-                              'Overlap probability'])['runtime'].mean()
-code1_runtime = runtime_data['code1']
-code1_runtime.name = 'Code 1'
-code2_runtime = runtime_data['code2']
-code2_runtime.name = 'Code 2'
-
-fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={'projection': '3d'})
-plt.tight_layout(pad=7)
-
-num_exams = df['Number of exams'].unique()
-for exams in num_exams:
-    code1_data = code1_runtime.xs(exams, level='Number of exams')
-    code2_data = code2_runtime.xs(exams, level='Number of exams')
-
-    ax1.scatter(code1_data.index,  # Probability
-                np.repeat(exams, len(code1_data)),  # Number of exams
-                code1_data)  # Runtime
-    ax2.scatter(code2_data.index,
-                np.repeat(exams, len(code2_data)),
-                code2_data)
-
-ax1.set_xlabel('Overlap probability')
-ax1.set_ylabel('Number os exams')
-ax1.set_zlabel('Runtime (s)')
-
-ax2.set_xlabel('Overlap probability')
-ax2.set_ylabel('Number os exams')
-ax2.set_zlabel('Runtime (s)')
-
-plt.savefig('img/runtime_3d.pdf', format='pdf')
