921d2b1522b2d10be8727f275b6fcc7e49ca2bf7
Marcio Lima <marcio.lima.inacio@gmail.com>
2021-11-03 19:13:50 -0300

    Add grid plot

--- visualization.py
+++ visualization.py
@@ -26,22 +26,54 @@ df['Seed'] = df['Seed'].astype('int')
 #### Plots ####
 
 # Timeout histogram
-num_slots_data = df.groupby(by=['algorithm'])['slots'].value_counts()
-num_slots_counts = num_slots_data.index.get_level_values('slots')
+# num_slots_data = df.groupby(by=['algorithm'])['slots'].value_counts()
+# num_slots_counts = num_slots_data.index.get_level_values('slots')
 
-timeout_counts = num_slots_data[num_slots_counts == -1]
-timeout_counts.index = timeout_counts.index.droplevel(1)
-timeout_counts.name = 'Timeout'
-non_timeout_counts = num_slots_data[num_slots_counts != -1]
-non_timeout_counts = non_timeout_counts.groupby('algorithm').sum()
-non_timeout_counts.name = 'Non-timeout'
+# timeout_counts = num_slots_data[num_slots_counts == -1]
+# timeout_counts.index = timeout_counts.index.droplevel(1)
+# timeout_counts.name = 'Timeout'
+# non_timeout_counts = num_slots_data[num_slots_counts != -1]
+# non_timeout_counts = non_timeout_counts.groupby('algorithm').sum()
+# non_timeout_counts.name = 'Non-timeout'
 
-timeout_df = pd.concat([timeout_counts, non_timeout_counts], axis=1)
-timeout_df.index = ['Code 1', 'Code 2']
-ax = timeout_df.plot.bar()
-for p in ax.patches:
-    ax.annotate(str(p.get_height()),
-                (p.get_x() + (p.get_width() * 0.5), p.get_height() * 1.005),
-                ha='center')
-plt.tight_layout()
-plt.savefig('img/timeout_hist.pdf', format='pdf')
+# timeout_df = pd.concat([timeout_counts, non_timeout_counts], axis=1)
+# timeout_df.index = ['Code 1', 'Code 2']
+# ax = timeout_df.plot.bar()
+# for p in ax.patches:
+#     ax.annotate(str(p.get_height()),
+#                 (p.get_x() + (p.get_width() * 0.5), p.get_height() * 1.005),
+#                 ha='center')
+# plt.tight_layout()
+# plt.savefig('img/timeout_hist.pdf', format='pdf')
+
+# Grid scatter plot
+code1_df = df.loc[df['algorithm'] == 'code1']
+code2_df = df.loc[df['algorithm'] == 'code2']
+
+fig, axs = plt.subplots(2, 3,
+                        figsize=(15, 10))
+fig.suptitle('Code 1', fontsize=16)
+
+for i, prob in enumerate(df['Overlap probability'].unique()):
+    ax = axs[i // 3][i % 3]
+    prob_df = code1_df.loc[code1_df['Overlap probability'] == prob]
+    ax.scatter(prob_df['Number of exams'], prob_df['runtime'],
+               marker='o', alpha=0.3)
+    ax.set_xlabel('Number of exams')
+    ax.set_ylabel('Runtime')
+    ax.set_title(f'Probability {prob}')
+plt.savefig('img/grid_scatter_code1.pdf', format='pdf')
+
+fig, axs = plt.subplots(2, 3,
+                        figsize=(15, 10))
+fig.suptitle('Code 2', fontsize=16)
+
+for i, prob in enumerate(df['Overlap probability'].unique()):
+    ax = axs[i // 3][i % 3]
+    prob_df = code2_df.loc[code2_df['Overlap probability'] == prob]
+    ax.scatter(prob_df['Number of exams'], prob_df['runtime'],
+               marker='o', alpha=0.3)
+    ax.set_xlabel('Number of exams')
+    ax.set_ylabel('Runtime')
+    ax.set_title(f'Probability {prob}')
+plt.savefig('img/grid_scatter_code2.pdf', format='pdf')
