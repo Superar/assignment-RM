b8b2854f63f39a19e126a5a2f2277392bacce333
Marco <marco_silva10@hotmail.com>
2021-11-03 11:15:29 +0000

    Bugs found in code1.c and code2.c corrected
    Two bugs were found in code1.c and code2.c. 
    The particular cases are:
    When the minimum number of time slots is 1 (the program may report 2)
    Exceptional cases in which the minimum number of time slots is plus one or minus one that that of reported by the previous version. You can only identify these cases by repeating the experiment with the new codes using the same random seed. 
    The running time of 1) should not be changed. In case 2), there might exist some small variation on the running time, since it may give rise to plus one or minus one recursive call. This variation should be, anyway, very small.

--- material/code1.c
+++ material/code1.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define NMAX 300
 
@@ -14,107 +14,103 @@ clock_t start, end;
 int seed, max_time;
 
 void shuffle(int n, int seed) {
-   int i;
+  int i;
 
-   if (n > 1) {
-  	srand(seed);
-        for (i = 0; i <= n - 1; i++) {
-        	int j = i + rand() / (RAND_MAX / (n - i) + 1);
-          	int t = p[j];
-          	p[j] = p[i];
-          	p[i] = t;
-        }
+  if (n > 1) {
+    srand(seed);
+    for (i = 0; i <= n - 1; i++) {
+      int j = i + rand() / (RAND_MAX / (n - i) + 1);
+      int t = p[j];
+      p[j] = p[i];
+      p[i] = t;
     }
+  }
 }
 
-
-double backtrack(int a[], int pos, int col, int n){
+double backtrack(int a[], int pos, int col, int n) {
   int i;
-  int c[NMAX];
-  
+  int c[NMAX + 1];
+
   end = clock();
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;	
- 
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
   /* complete solution found*/
-  if (pos == n){
-	found = 1;
-    	return cpu_time_used;
+  if (pos == n) {
+    found = 1;
+    return cpu_time_used;
   }
- if (cpu_time_used > max_time) {
-	found = -1;
-	return cpu_time_used;
+  if (cpu_time_used > max_time) {
+    found = -1;
+    return cpu_time_used;
   }
 
+  for (i = 1; i <= col; i++)
+    c[i] = 1;
 
-  /* initialize possible colors*/
-  for (i = 0; i <= col; i++)
-  	c[i] = 1;
-  
-  /* delete colors that are used by adjacent nodes*/
-  for (i = 0; i < pos; i++){
-    	if (M[p[pos]][p[i]] == 1){    
-		c[a[p[i]]] = 0;        
-    	}
+  for (i = 0; i < pos; i++) {
+    if (M[p[pos]][p[i]] == 1) {
+      c[a[p[i]]] = 0;
+    }
   }
-  
-  /* insert a color in the node.*/
-  for (i = 0; i < col; i++) {
-    	if (c[i] == 1){  
-      		a[p[pos]] = i;
-      		/* if a coloring was found:*/
-      		cpu_time_used = backtrack(a, pos + 1, col, n);
-      		if ((found == 1) || (found == -1))
-			return cpu_time_used;
-    	}
+
+  for (i = 1; i <= col; i++) {
+    if (c[i] == 1) {
+      a[p[pos]] = i;
+      cpu_time_used = backtrack(a, pos + 1, col, n);
+      if ((found == 1) || (found == -1))
+        return cpu_time_used;
+    }
   }
   return cpu_time_used;
 }
-
 
 int main(int argc, char *argv[]) {
   int i, j, x, y, n, m, c;
   int a[NMAX];
 
   seed = atoi(argv[1]);
-  max_time = atoi(argv[2]);	
+  max_time = atoi(argv[2]);
   fp = fopen(argv[3], "r");
-  
-  if (fp == NULL){
-      perror("Error while opening the file.\n");
-      exit(EXIT_FAILURE);
+
+  if (fp == NULL) {
+    perror("Error while opening the file.\n");
+    exit(EXIT_FAILURE);
   }
 
-  fscanf(fp,"%d %d", &n, &m);
-  for (i = 0; i < n; i++){
-      a[i] = 0;
-      p[i] = i;
-      for (j = 0; j < n; j++)
-	M[i][j] = 0;
-   }    
-   shuffle(n, seed);	  
+  fscanf(fp, "%d %d", &n, &m);
+  for (i = 0; i < n; i++) {
+    a[i] = 0;
+    p[i] = i;
+    for (j = 0; j < n; j++)
+      M[i][j] = 0;
+  }
+  shuffle(n, seed);
 
-   for (i = 0; i < m; i++){
-      fscanf(fp,"%d %d", &x, &y);
-      M[x-1][y-1] = M[y-1][x-1] = 1;
-   }    
-   start = clock();
+  for (i = 0; i < m; i++) {
+    fscanf(fp, "%d %d", &x, &y);
+    M[x - 1][y - 1] = M[y - 1][x - 1] = 1;
+  }
+  start = clock();
 
-   c = n;
-   while(c--) {
-      found = 0;
-      a[0] = 1;
-      cpu_time_used = backtrack(a,1,c,n);
-      if (found == 0){   // found the minimum with c+1
-	printf("%d ",c+1);
-   	printf("%lf \n", cpu_time_used);	
-	break;
-      }
-      else if (found == -1) {   // amount of time exceeded
-  	printf("-1 ");
-   	printf("%lf \n", cpu_time_used);	
-	break;
-      }
-   }
+  c = n;
+  while (--c) {
+    found = 0;
+    a[p[0]] = 1;
+    cpu_time_used = backtrack(a, 1, c, n);
+    if (found == 0) { // found the minimum with c+1
+      printf("%d ", c + 1);
+      printf("%lf \n", cpu_time_used);
+      break;
+    } else if (found == -1) { // amount of time exceeded
+      printf("-1 ");
+      printf("%lf \n", cpu_time_used);
+      break;
+    }
+  }
+  if (c == 0) {
+    // If c is 0, then 1 was possible.
+    printf("1 %lf \n", cpu_time_used);
+  }
 
-   return 0;
+  return 0;
 }
